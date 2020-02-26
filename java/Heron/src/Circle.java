7c1044c9ccf4e3d65a5334b6934e4434ff791129
joro2404 <joro2404@abv.bg>
2020-02-26 16:44:33 +0200

    java intro

---
+++ java/Heron/src/Circle.java
@@ -0,0 +1,31 @@
+public class Circle {
+
+    private Point x;
+    private Integer r;
+
+
+    public Circle(Point x, Integer r){
+        this.x = x;
+        this.r = r;
+    }
+
+    public Point getX() {
+        return x;
+    }
+
+    public void setX(Point x) {
+        this.x = x;
+    }
+
+    public Integer getR() {
+        return r;
+    }
+
+    public void setR(Integer r) {
+        this.r = r;
+    }
+
+    public Integer get_Perimeter(){
+        
+    }
+}
